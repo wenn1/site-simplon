9b8f2599de61948758b7c772acd1f3bb9ec5a913
wenn1 <nolwenn.repellin@orange.fr>
2017-05-24 13:26:50 +0200

    Update inc_skip.php

--- siteNolwenn/include/inc_skip.php
+++ siteNolwenn/include/inc_skip.php
@@ -1,7 +1,6 @@
-
 <!-- Menu d'accessibilité -->
-							<ul>
-									<li><a href="#main" title="Aller au contenu de la page">contenu</a></li>
-									<li><a href="#menu" title="Aller au menu principal">menu</a></li>
-                  <li><a href="#footer" title="Aller au pied de page">pied de page</a></li>
-							</ul>
+<ul>
+	<li><a href="#main" title="Aller au contenu de la page">contenu</a></li>
+	<li><a href="#menu" title="Aller au menu principal">menu</a></li>
+	<li><a href="#footer" title="Aller au pied de page">pied de page</a></li>
+</ul>
