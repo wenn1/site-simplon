fe57504c9d43b1b384eb6144c5920ccba4c8bc0e
Benoit <benoitlagrange38@hotmail.fr>
2017-05-24 15:06:37 +0200

    modification Formation1

---
+++ siteNolwenn/LaFormation.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+			<?php include('include/inc_head.php') ?>
+			<meta name="description" content="titre de la page"/>
+
+			<title>404</title>
+
+	</head>
+
+	<body role="document">
+			<div id="content">
+					<nav id="skip"><?php include('include/inc_skip.php') ?>
+					</nav>
+
+					<header id="header" role="banner">
+							<?php include('include/inc_header.php') ?>
+					</header>
+
+					<nav id="menu" role="navigation"><?php include('include/inc_menu.php') ?>
+					</nav>
+<section >
+		<h1>Develo'Pont ? Qu'est-ce que c'est ? </h1>
+		
+		<p> 
+		Cette formation entre dans un double dispositif : le contrat d'aide au retour à l'emploi durable (Cared), financé par la Région et POEC : 
+		préparation opérationnelle à l'emploi collectif financée par OPCALIA. Elle sera encadrée par les bénévoles de l'EPN, deux formateurs, 
+		une administratrice et une chargée des relations entreprise, tous Pontois.
+		</p>
+		
+		<h1>Combien de temps dure la formation ?</h1>
+		
+		<p>
+		6 mois à temps plein sur le centre de formation (avec ou sans 6 mois de stage supplémentaire) : 
+		vous êtes à temps plein (35h) sur votre centre et vous avez la possibilité à la fin de la formation de
+		faire 6 mois de stage pour compléter votre apprentissage.
+		</p>		
+		 
+		<h1>Qu’est-ce que vous faîtes pendant les 6 mois de formation ?</h1>
+		
+		<p>
+		<span class="gris"> Chaque centre de formation a sa spécialité de langage.</span> 
+		Consultez les différents programmes ci-dessous pour en savoir plus :
+		</p>
+		
+		<table>
+			<tr>
+				<td id="fs">FullStack JS</td>
+				<td id="php">PHP</td>
+				<td id="ruby">RUBY</td>
+				<td id="python">PYTHON</td>
+			</tr>
+			<tr>
+				<td> 
+					 <ul>
+ 						<li> <span>Des compétences et des technologies :</span> <span class="gris"> cette formation est dédiée au développement « front-end » </span>  (ce qu’on voit à l’écran, avec HTML, CSS, Javascript et ses différents frameworks comme Meteor, React, etc). Vous verrez tout de même un peu de développement « back-end » (les coulisses, les fonctionnalités), l’administration systèmes (sous Linux), la programmation d’objets connectés, la gestion de projet et la conception d’interfaces.</li>
+ 						<li> <span>Des méthodes :</span> méthodologies agiles, pair programming, etc.</li>
+ 						<li> <span>Des outils : </span>Git, Github, Slack, Sublime text, etc.</li>
+ 						<li> <span>Des ouvertures sur d’autres domaines :</span> Sécurité, environnement, intelligence artificielle, consommation collaborative, entrepreneuriat, communication, marketing, … Autant de sujets qui pourront donner lieu à des interventions, sur votre demande notamment.</li>
+ 						<li> <span>Nouveau dès novembre 2015 :</span> possibilité de faire certifier sa formation au travers d’un titre professionnel inscrit au RNCP (Développeur logiciel, niveau III)</li>
+ 					</ul>
+				</td>
+
+				<td>
+ 					<ul>
+ 						 <li> <span> Des compétences et des technologies :</span> vous ferez un mélange de développement <span class="gris"> « front-end » </span> (ce qu’on voit à l’écran, avec HTML, CSS, Javascript), le développement <span class="gris"> « back-end » </span> (les coulisses, les fonctionnalités) <span class="gris"> avec le langage </span> <span> PHP </span>, l’administration systèmes (sous Linux), la programmation d’objets connectés, la gestion de projet et la conception d’interfaces.</li>
+ 						 <li> <span>Des méthodes :</span> méthodologies agiles, pair programming, etc.</li>
+ 						 <li> <span>Des outils :</span> Git, Github, Slack, Sublime text, etc.</li>
+ 						 <li> <span>Des ouvertures sur d’autres domaines :</span> Sécurité, environnement, intelligence artificielle, consommation collaborative, entrepreneuriat, communication, marketing, … Autant de sujets qui pourront donner lieu à des interventions, sur votre demande notamment.</li>
+ 						 <li> <span>Nouveau dès novembre 2015 :</span> possibilité de faire certifier sa formation au travers d’un titre professionnel inscrit au RNCP (Développeur logiciel, niveau III)</li>
+ 					</ul>
+				</td>
+
+				<td> 
+ 					<ul>
+ 						<li> <span>Des compétences et des technologies :</span> vous ferez un mélange de développement <span class="gris"> « front-end » </span> (ce qu’on voit à l’écran, avec HTML, CSS, Javascript), le développement <span class="gris"> « back-end » </span> (les coulisses, les fonctionnalités) <span class="gris"> avec le langage </span> <span> Ruby </span>, l’administration systèmes (sous Linux), la programmation d’objets connectés, la gestion de projet et la conception d’interfaces.</li>
+ 						<li> <span>Des méthodes :</span> méthodologies agiles, pair programming, etc.</li>
+ 						<li> <span>Des outils :</span> Git, Github, Slack, Sublime text, etc.</li>
+ 						<li> <span>Des ouvertures sur d’autres domaines :</span> Sécurité, environnement, intelligence artificielle, consommation collaborative, entrepreneuriat, communication, marketing, … Autant de sujets qui pourront donner lieu à des interventions, sur votre demande notamment.</li>
+ 						<li> <span>Nouveau dès novembre 2015 :</span> possibilité de faire certifier sa formation au travers d’un titre professionnel inscrit au RNCP (Développeur logiciel, niveau III)</li>
+ 					</ul>
+				</td>
+
+				<td>
+ 					<ul>
+ 						<li> <span>Des méthodes :</span> méthodologies agiles, pair programming, etc.</li>
+ 						<li> <span>Des outils :</span> Git, Github, Slack, Sublime text, etc.</li>
+ 						<li> <span>Des ouvertures sur d’autres domaines :</span> Sécurité, environnement, intelligence artificielle, consommation collaborative, entrepreneuriat, communication, marketing, … Autant de sujets qui pourront donner lieu à des interventions, sur votre demande notamment.</li>
+ 						<li> <span>Nouveau dès novembre 2015 :</span> possibilité de faire certifier sa formation au travers d’un titre professionnel inscrit au RNCP (Développeur logiciel, niveau III)</li>
+					</ul>
+				</td>
+
+			</tr>
+		</table>	
+		
+		<h1>Quels débouchés après la formation ?</h1>
+		
+		<ul>
+			<li> <span class="gris"> Développeur junior</span>, pour continuer à se former au sein d’une équipe</li>
+			<li><span class="gris">Intégrateur web </span>, UX designer…</li>
+			<li>Ou auront la possibilité de mettre à profit les compétences techniques acquises pendant la formation pour exercer d’autres métiers :<span class="gris"> chef de projet web, formateur ou médiateur numérique </span>, community manager…</li>	
+		</ul>
+		
+	</section>
+					<article id="main" role="main">
+								<h3>Erreur 404</h3>
+									<section class="exercice html">
+										<h4>Erreur 404</h4>
+									</section>
+					</article>
+					<aside id="aside" role="complementary"><?php include('include/inc_aside.php') ?>
+					</aside>
+					<footer id="footer" role="contentinfo"><?php include('include/inc_footer.php') ?>
+					</footer>
+
+			</div><!-- fin #content -->
+	</body>
+</html>
