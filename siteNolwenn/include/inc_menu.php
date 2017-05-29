1de6a877c794f8cfecc4bebb8331c1522a0834cd
wattmetre <fwillhelm@gmail.com>
2017-05-29 11:13:22 +0200

    modif actualites=lapromoleslocaux

--- siteNolwenn/include/inc_menu.php
+++ siteNolwenn/include/inc_menu.php
@@ -2,7 +2,7 @@
 <ul>
 		<li class="li"><a href="index.php" >Accueil</a></li >
 		<li class="li" ><a href="LaFormation.php" >La formation</a></li>
-		<li class="li"><a href="actualites.php">Actualités</a></li>
+		<li class="li"><a href="lapromoleslocaux.php">La Promo/Les Locaux</a></li>
 		<li class="li"><a href="galerie.php">Galerie photos</a></li>
 		<li class="li"><a href="contact.php">Contact</a></li>
 </ul>
