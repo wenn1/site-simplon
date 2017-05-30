7441579e267a80cf195e3f3b787fee21dff176f5
mylène <dragosta0813@live.fr>
2017-05-30 09:29:10 +0200

    ajout lien nouvelle fenetre

--- siteNolwenn/include/inc_footer.php
+++ siteNolwenn/include/inc_footer.php
@@ -2,16 +2,16 @@
 <p id="valid"><a href="./" title="retour à l'accueil"><img src="css/img/logo-developont.png" alt="logo Develo'pont"/></a></p>
 
 <ul class="lien_site">
-	<li><a href="http://simplon.co/" title="simplon">Site de Simplon</a></li>
-	<li><a href="http://developponts.enpc.org/" title="developont">Site de Dévelo'pont</a></li>
-	<li><a href="http://vercors-pontenroyans.com/" title="pontenroyans">Site de Pont-en-Royans</a></li>
-	<li><a href="https://www.legrand.fr/" title="legrand">Site de Legrand</a></li>
-	<li><a href="https://fr-fr.facebook.com/Simplon.co/" title="simplonfb">Facebook de Simplon</a></li>
-	<li><a href="https://www.facebook.com/developont/?hc_ref=PAGES_TIMELINE&fref=nf" title="developontfb">Facebook de Dévelo'pont</a></li>
-	<li><a href="https://twitter.com/simplonco?lang=fr" title="simplontw">Twitter de Simplon</a></li>
-	<li><a href="https://twitter.com/developont?lang=fr" title="developonttw">Twitter de Dévelo'pont</a></li>
-	<li><a href="https://www.epn-du-royans.fr/" title="epn">EPN de Pont-en-Royans</a></li>
-	<li><a href="http://www.europe-centre.eu/fr/42/Le_programme_LEADER.html" title="leader">Le Programme LEADER</li>
+	<li><a href="http://simplon.co/" title="simplon" target="_blank">Site de Simplon</a></li>
+	<li><a href="http://developponts.enpc.org/" title="developont" target="_blank">Site de Dévelo'pont</a></li>
+	<li><a href="http://vercors-pontenroyans.com/" title="pontenroyans" target="_blank">Site de Pont-en-Royans</a></li>
+	<li><a href="https://www.legrand.fr/" title="legrand" target="_blank">Site de Legrand</a></li>
+	<li><a href="https://fr-fr.facebook.com/Simplon.co/" title="simplonfb" target="_blank">Facebook de Simplon</a></li>
+	<li><a href="https://www.facebook.com/developont/?hc_ref=PAGES_TIMELINE&fref=nf" title="developontfb" target="_blank">Facebook de Dévelo'pont</a></li>
+	<li><a href="https://twitter.com/simplonco?lang=fr" title="simplontw" target="_blank">Twitter de Simplon</a></li>
+	<li><a href="https://twitter.com/developont?lang=fr" title="developonttw" target="_blank">Twitter de Dévelo'pont</a></li>
+	<li><a href="https://www.epn-du-royans.fr/" title="epn" target="_blank">EPN de Pont-en-Royans</a></li>
+	<li><a href="http://www.europe-centre.eu/fr/42/Le_programme_LEADER.html" title="leader" target="_blank">Le Programme LEADER</a></li>
 </ul>
 
 <p> © 2017 </p>
