dc93a2655c061a0b1a66a77b4f9ba184913232f8
nolwenn <nolwenn.repellin@orange.fr>
2017-06-01 15:58:56 +0200

    suppression menu accessibilite

--- siteNolwenn/include/inc_skip.php
+++
@@ -1,6 +0,0 @@
-<!-- Menu d'accessibilité -->
-<ul>
-	<li><a href="#ancre_main" title="Aller au contenu de la page">contenu</a></li>
-	<li><a href="#ancre_menu" title="Aller au menu principal">menu</a></li>
-	<li><a href="#footer" title="Aller au pied de page">pied de page</a></li>
-</ul>
